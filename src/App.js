fe0a0b987ae5bb021cc479249d3bf88bef618e76
Boris Muzalevskii <bmuza57@gmail.com>
2022-10-02 15:01:55 +0300

    Create Container and NewsList component

--- src/App.js
+++ src/App.js
@@ -3,28 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import { dataFromServer } from './api mock/data';
-
-console.log('data', dataFromServer);
+import Containner from './components/NewsList/Container';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'>
+    <Containner />
+  </div>;
 }
 
 export default App;
---
+++ src/components/NewsList/Container/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Containner = () => {
+  return <div>Containner</div>;
+};
+
+export default Containner;
