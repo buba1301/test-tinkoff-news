87f89bffc0a1ad7fcead586fdcaf954eb6097859
Boris Muzalevskii <bmuza57@gmail.com>
2022-10-02 20:29:26 +0300

    Add button for slider

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -5,7 +5,21 @@ import s from './Container.module.css';
 const Containner = () => {
   return (
     <div className={s.newsContainer}>
-      <div className={s.newsWrap}>Containner</div>
+      <div className={s.newsWrap}>
+        <div className={s.header}>
+          <h1>Для вас</h1>
+          <div className={s.sliderArrows}>
+            <div className={s.arrowButtonsWrap}>
+              <button></button>
+            </div>
+            <div className={s.arrowButtonsWrap}>
+              <button></button>
+            </div>
+          </div>
+        </div>
+        <div>Contant</div>
+        <div>Navigation</div>
+      </div>
     </div>
   );
 };
